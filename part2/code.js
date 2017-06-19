120d8086c8627dc787102fe1ac320adc54a72275
David Reignier <david.reignier@gmail.com>
2017-06-19 13:19:46 +0200

    Add part2

---
+++ part2/code.js
@@ -0,0 +1,16 @@
+function job() {
+    return new Promise(function(resolve, reject) {
+        setTimeout(resolve, 500, 'Hello world 1');
+    });
+}
+
+async function test() {
+    let message = await job();
+    console.log(message);
+
+    return 'Hello world 2';
+}
+
+test().then(function(message) {
+    console.log(message);
+});
---
+++ part2/statement.md
@@ -0,0 +1,4 @@
+Take a look at the following code and run it.
+
+@[Asynchronous code using setTimeout]({"stubs":["code.js"], "command":"node code.js", "project":"part"})
+
